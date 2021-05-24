7a8e378ce80fd259322479f1f2c4a7c3edd23f7e
milkon <melkon_2010@hotmail.com>
2021-05-24 15:49:13 +0200

    added more styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,19 @@
+import { Link } from "gatsby"
 import React from "react"
 import Layout from "../components/Layout"
+import * as styles from "../styles/home.module.css"
 
 export default function Home() {
   return (
     <Layout>
-      <section>
+      <section className={styles.header}>
         <div>
           <h2>Design</h2>
           <h3>Develop & Deploy</h3>
           <p>UX designer & web developer based in Manchester</p>
+          <Link to="/projects" className={styles.btn}>
+            My Portfolio Projects
+          </Link>
         </div>
       </section>
     </Layout>
--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,10 +1,14 @@
 import React from "react"
 import Layout from "../../components/Layout"
+import * as styles from "../../styles/projects.module.css"
 
-export default function index() {
+export default function Projects() {
   return (
     <Layout>
-      <div>index</div>
+      <div className={styles.portfolio}>
+        <h2>Protfolio</h2>
+        <h3>Projects & Websites i've Created</h3>
+      </div>
     </Layout>
   )
 }
