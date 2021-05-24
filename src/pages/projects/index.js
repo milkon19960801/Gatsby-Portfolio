e15eae77061be4a03722cf13534fa3134572d794
milkon <melkon_2010@hotmail.com>
2021-05-24 21:14:57 +0200

    added images for projects

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,44 +1,56 @@
-import { graphql, Link } from "gatsby"
 import React from "react"
 import Layout from "../../components/Layout"
 import * as styles from "../../styles/projects.module.css"
+import { Link, graphql } from "gatsby"
+import Img from "gatsby-image"
 
-export default function Projects({ data }) {
+const Projects = ({ data }) => {
   console.log(data)
   const projects = data.projects.nodes
-  const { contact } = data.contact.siteMetadata
+  const contact = data.contact.siteMetadata.contact
+
   return (
     <Layout>
       <div className={styles.portfolio}>
-        <h2>Protfolio</h2>
-        <h3>Projects & Websites i've Created</h3>
+        <h2>Portfolio</h2>
+        <h3>Projects & Websites I've Created</h3>
         <div className={styles.projects}>
           {projects.map(project => (
-            <Link to={`/projects/${project.frontmatter.slug}`} key={project.id}>
+            <Link to={"/projects/" + project.frontmatter.slug} key={project.id}>
               <div>
+                <Img fluid={project.frontmatter.thumb.childImageSharp.fluid} />
                 <h3>{project.frontmatter.title}</h3>
                 <p>{project.frontmatter.stack}</p>
               </div>
             </Link>
           ))}
         </div>
-        <p>Like what you see? Email me att {contact} for a quote!</p>
+        <p>Likewhat you see? email me at {contact} for a quote!</p>
       </div>
     </Layout>
   )
 }
 
-//export page query
+export default Projects
+
+// export page query
 export const query = graphql`
   query ProjectsPage {
     projects: allMarkdownRemark(
-      sort: { fields: frontmatter___date, order: DESC }
+      sort: { order: DESC, fields: frontmatter___date }
     ) {
       nodes {
         frontmatter {
-          title
-          stack
           slug
+          stack
+          title
+          thumb {
+            childImageSharp {
+              fluid {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
         }
         id
       }
