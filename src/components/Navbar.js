fb74516c3c2e0332b657c70712f75e290a60f8e3
milkon <melkon_2010@hotmail.com>
2021-05-24 16:50:07 +0200

    useing static queries

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,22 @@
-import { Link } from "gatsby"
+import { Link, graphql, useStaticQuery } from "gatsby"
 import React from "react"
 
 export default function Navbar() {
+  const data = useStaticQuery(graphql`
+    {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
+
+  const { title } = data.site.siteMetadata
+
   return (
     <nav>
-      <h1>Web Warrior</h1>
+      <h1>{title}</h1>
       <div className="links">
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,9 @@
-import { graphql, Link } from "gatsby"
+import { Link } from "gatsby"
 import React from "react"
 import Layout from "../components/Layout"
 import * as styles from "../styles/home.module.css"
 
-export default function Home({ data }) {
-  console.log(data)
-  const { title, description } = data.site.siteMetadata
+export default function Home() {
   return (
     <Layout>
       <section className={styles.header}>
@@ -19,22 +17,7 @@ export default function Home({ data }) {
         </div>
 
         <img src="/banner.png" alt="site banner" style={{ maxWidth: "100%" }} />
-
-        <p>
-          {title} - {description}
-        </p>
       </section>
     </Layout>
   )
 }
-
-export const query = graphql`
-  query SiteInfo {
-    site {
-      siteMetadata {
-        description
-        title
-      }
-    }
-  }
-`
