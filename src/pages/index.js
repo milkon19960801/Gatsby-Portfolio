db17427cd65d2966a38f4945130fd1501d62a3cf
milkon <melkon_2010@hotmail.com>
2021-05-24 16:19:51 +0200

    added a static file

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,6 +15,8 @@ export default function Home() {
             My Portfolio Projects
           </Link>
         </div>
+
+        <img src="/banner.png" alt="site banner" style={{ maxWidth: "100%" }} />
       </section>
     </Layout>
   )
