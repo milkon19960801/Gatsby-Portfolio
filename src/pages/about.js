fcd3199e2522df621dae2d99a2a67ee26a454d64
milkon <melkon_2010@hotmail.com>
2021-05-24 13:12:16 +0200

    pageing with gatsby

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default function about() {
+  return (
+    <div>
+      <h1>About page</h1>
+      <p>Lorem ipsum</p>
+      <p>Lorem ipsum</p>
+      <p>Lorem ipsum</p>
+    </div>
+  )
+}
